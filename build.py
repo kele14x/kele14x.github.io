fc0a26e9f28428c7142d4cc00539afe90f934fc8
Michiru Morishita <kokser.merio@gmail.com>
2021-06-25 15:32:24 +0800

    restruct folders

--- build.py
+++ build.py
@@ -1,29 +1,14 @@
+#!/usr/bin/python
 import os
 import subprocess
-
-source_dirs = ['posts']
-source_files = ['.md', '.rst']
-
-
-def list_sources(src_dirs: str):
-    for dir in src_dirs:
-        for dirpath, _, filenames in os.walk(dir):
-            for filename in filenames:
-                _, ext = os.path.splitext(filename)
-                if ext.lower() in source_files:
-                    yield os.path.join(dirpath, filename)
+import shutil
+import frontmatter
+from typing import List, Tuple
 
 
-def target_path(src_path: str) -> str:
-    """Get target file path from source file path."""
-    newpath, _ = os.path.splitext(src_path)
-    return newpath + '.html'
-
-
-def convert_file(target_path: str, *src_path: str):
+def convert_file(source: str, target: str):
     """Convert file to HTML using pandoc."""
-    s = ' '.join(src_path)
-    cmd = f'pandoc --standalone -o {target_path} {s}'
+    cmd = f'pandoc --standalone -o {target} {source}'
     print(f'Run command: {cmd}')
     try:
         subprocess.run(cmd)
@@ -31,31 +16,69 @@ def convert_file(target_path: str, *src_path: str):
         print(e)
 
 
-def generate_index(files: tuple[str, str]):
+def generate_index(index: List[Tuple], target: str):
     """Generate index.md, which should contains list of all posts."""
-    with open('index.md', 'w') as f:
+    print(f'Generate file {target}')
+    with open(target, 'w', encoding='utf-8', newline='\n') as f:
         f.write(
             '---\n'
             'title: Kele\'s Blog\n'
-            '...\n\n'
+            '---\n\n'
         )
-        for src, trg in files:
-            filename = os.path.basename(src)
-            filename, _ = os.path.splitext(filename)
-            filename = filename.replace('-', ' ').title()
-            f.write(f'- [{filename.title()}](./{trg})\n\n')
+        for title, path in index:
+            f.write(f'- [{title}](./{path})\n\n')
+
+
+def build(source: str, target: str) -> List[Tuple]:
+    posts = []
+
+    for dirpath, dirnames, filenames in os.walk(source):
+        tardir = dirpath.replace(os.path.basename(source), target, 1)
+
+        # Duplicate folder structure
+        for dirname in dirnames:
+            p = os.path.join(tardir, dirname)
+            print(f'Create folder {p}')
+            if not os.path.lexists(p):
+                os.mkdir(os.path.join(tardir, dirname))
+            elif not os.path.isdir(p):
+                raise FileExistsError(f'{p} already exists but is not directory')
+
+        # Copy or convert file
+        for filename in filenames:
+            root, ext = os.path.splitext(filename)
+            s = os.path.join(dirpath, filename)
+            if ext.lower() in ['.md']:
+                t = os.path.join(tardir, root) + '.html'
+                # Load frontmatter
+                fm = frontmatter.load(s)
+                title = fm['title']
+                url = os.path.relpath(t, target).replace('\\', '/')
+                posts.append((title, url))
+                # Convert file
+                print(f'Convert file {s} -> {t}')
+                convert_file(s, t)
+            else:
+                t = os.path.join(tardir, filename)
+                print(f'Copy file {s} -> {t}')
+                shutil.copyfile(s, t)
+
+    return posts
 
 
 if __name__ == '__main__':
-    curdir = os.path.dirname(__file__)
-    os.chdir(curdir)
-    print(f'Working on directory: {curdir}')
+    current_dir = os.path.dirname(__file__)
+    os.chdir(current_dir)
+    print(f'Working on directory: {current_dir}')
 
-    files = [(src, target_path(src)) for src in list_sources(source_dirs)]
-    print(f'{len(files)} files found')
+    plist = build('posts', 'docs')
+    build('assets', 'docs')
 
-    generate_index(files)
-    convert_file('index.html', 'index.md')
+    # Generate index.md and convert it to index.html
+    index_source = os.path.join('docs', 'index.md')
+    index_target = os.path.join('docs', 'index.html')
+    generate_index(plist, index_source)
+    print(f'Convert file {index_source} -> {index_target}')
+    convert_file(index_source, index_target)
 
-    for src, trg in files:
-        convert_file(trg, src)
+    print('All done')
